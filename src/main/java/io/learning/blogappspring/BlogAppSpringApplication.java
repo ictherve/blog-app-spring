07da6a52689d24196f8c49c10626ab38d9991efa
Ekem Herve <ekemherve@gmail.com>
2019-07-22 12:48:21 +0200

    Initial commit

---
+++ src/main/java/io/learning/blogappspring/BlogAppSpringApplication.java
@@ -0,0 +1,13 @@
+package io.learning.blogappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAppSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/learning/blogappspring/BlogAppSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package io.learning.blogappspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogAppSpringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
