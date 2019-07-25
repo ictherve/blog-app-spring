1be66abb89ce677fff04e2b7dce1038ee6731569
Ekem Herve <ekemherve@gmail.com>
2019-07-25 11:50:15 +0200

    Add Role

---
+++ src/main/java/io/learning/blogappspring/model/Role.java
@@ -0,0 +1,25 @@
+package io.learning.blogappspring.model;
+
+import lombok.Data;
+
+import java.util.Objects;
+
+@Data
+public class Role {
+
+    private Long id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
