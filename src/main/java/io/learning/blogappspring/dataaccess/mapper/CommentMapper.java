daddd6d92c3978aacb1b96f1918660eabfd8372c
Ekem Herve <ekemherve@gmail.com>
2019-07-26 20:38:33 +0200

    Add Post and Comment Mapper

--- src/main/java/io/learning/blogappspring/dataaccess/mapper/CommentMapper.java
+++ src/main/java/io/learning/blogappspring/dataaccess/mapper/CommentMapper.java
@@ -1,4 +1,19 @@
 package io.learning.blogappspring.dataaccess.mapper;
 
-public class CommentMapper {
+import io.learning.blogappspring.dataaccess.entity.CommentEntity;
+import io.learning.blogappspring.model.Comment;
+import org.mapstruct.Mapper;
+
+import java.util.Collection;
+
+@Mapper(componentModel = "spring",uses = PostMapper.class)
+public interface CommentMapper {
+
+    Comment toModel(CommentEntity postEntity);
+
+    CommentEntity toEntity(Comment comment);
+
+    Collection<Comment> toCollection(Collection<CommentEntity> posts);
+
+    Collection<CommentEntity> toEntityCollection(Collection<Comment> comments);
 }
--- src/main/java/io/learning/blogappspring/dataaccess/mapper/PostMapper.java
+++ src/main/java/io/learning/blogappspring/dataaccess/mapper/PostMapper.java
@@ -1,4 +1,20 @@
 package io.learning.blogappspring.dataaccess.mapper;
 
+import io.learning.blogappspring.dataaccess.entity.PostEntity;
+import io.learning.blogappspring.model.Post;
+import org.mapstruct.Mapper;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.Collection;
+
+@Mapper(componentModel = "spring")
 public interface PostMapper {
+
+    Post toModel(PostEntity postEntity);
+
+    PostEntity toEntity(Post post);
+
+    Collection<Post> toCollection(Collection<PostEntity> posts);
+
+    Collection<PostEntity> toEntityCollection(Collection<Post> posts);
 }
